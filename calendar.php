a9331b463895c3e7054a3301e5a9f18a89480465
Cristian Romanescu <cristian@romanescu.ro>
2013-04-02 13:40:32 +0300

    Added URL to calendar as dynamic parameter. Everyone can use my URL now http://www.romanescu.ro/calendar?url=http://link_to_your_calendar

--- calendar.php
+++ calendar.php
@@ -1,6 +1,9 @@
 <?php
-$url = 'http://p09-calendarws.icloud.com/ca/subscribe/1/ABCDEFGHIJKLMNOPQ';
-$filename = 'cristiroma.ics';
+if(empty($_GET['url])) {
+    die('Please send the calendar as parameter to this page. ex. http://www.yoursite.com/calendar.php?url=http://p09-calendarws.icloud.com/ca/subscribe/1/ABCDEFGHIJKLMNOPQ
+}
+$url = $_GET['url'];
+$filename = 'calendar.ics';
 
 $data = file_get_contents($url);
 header("Content-Disposition: attachment; filename=\"$filename\"");
